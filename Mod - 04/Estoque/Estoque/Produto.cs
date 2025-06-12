b9f1d240620aabfd3af6382b98115ffa8676ec01
Gustavo Felipe Delgado <gustavofelipedelgado@hotmail.com>
2025-06-12 10:10:08 -0300

    12_06_2025_01

---
+++ Mod - 04/Estoque/Estoque/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque
+{
+    internal class Produto
+    {
+
+        public string Nome = "";
+        public double Preco = 0;
+        public int Quantidade = 0;
+
+        public double ValorTotalEmEstoque()
+        {
+            double Total = Quantidade * Preco;
+            return Total;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+            Console.WriteLine("Quantidade Adicionada");
+        }
+
+        public void RemoverProdutos(int quantidade) 
+        {
+            int Total = Quantidade - quantidade;
+
+            if(Total < 0)
+            {
+                Console.WriteLine("Saldo insuficiente de estoque!");
+            }
+            else{
+                Quantidade = Total;
+                Console.WriteLine("Saldo removido");
+            }
+        }
+
+        public override string ToString()
+        {
+           return Nome 
+           + "\n Preço R$" 
+           + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+           + "\n Quantidade em estoque: " + Quantidade 
+           + "\n Valor total de estoque: " 
+           + ValorTotalEmEstoque();
+        }
+
+    }
+}
---
+++ Mod - 04/Estoque/Estoque/Program.cs
@@ -0,0 +1,48 @@
+﻿using Estoque;
+using System.Globalization;
+
+string acesso = "";
+Produto produto = new Produto();
+
+
+Console.WriteLine("Bem vindo ao sistema de estoque!");
+Console.WriteLine("Insira os dados do produto:");
+
+Console.Write("Nome: ");
+produto.Nome = Console.ReadLine();
+Console.Write("Quantidade inicial: ");
+produto.Quantidade = int.Parse(Console.ReadLine());
+Console.Write("Preço: ");
+produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+while (acesso != "ext") {
+
+    Console.WriteLine();
+    Console.WriteLine("Dados do produto");
+    Console.WriteLine(produto.ToString());
+
+    Console.WriteLine("Comandos (add - 'adicionar' / rmv - 'remover' / ext - 'sair'");
+    acesso = Console.ReadLine();
+
+    
+
+    if (acesso == "rmv")
+    {
+        Console.WriteLine("Quantos produtos deseja remover?");
+        int quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        produto.RemoverProdutos(quantidade);
+
+    } else if (acesso == "add")
+    {
+        Console.WriteLine("Quantos produtos deseja adicionar?");
+        int quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        produto.AdicionarProdutos(quantidade);
+
+    }
+
+}
+
+Console.WriteLine();
+Console.WriteLine("Obrigado por usar o sistema de estoque!");
+
